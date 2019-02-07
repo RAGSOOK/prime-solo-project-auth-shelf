02d34489fb8cd78210afb67519fb62d958938d90
Tang Yang <ty4081097@gmail.com>
2019-02-07 15:21:19 -0600

    add get routes to get shelf.

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,7 +6,19 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+    if (req.isAuthenticated()) {
+        console.log('req.user', req.user);
+        pool.query('SELECT * FROM item;')
+        
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for shelf:', error);
+                res.sendStatus(500);
+            });
+        // if not a user then send this instead
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 
